aa917c190e3417a3e0f65905fe8a482edbced84b
Martin <martingarcia7@hotmail.com>
2023-05-26 19:04:08 -0400

    primer intento

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,6 +4,9 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+function getKeyByValue(books, value) {
+    return Object.keys(books).find(key => object[key].author === value);
+  }
 
 public_users.post("/register", (req,res) => {
   //Write your code here
@@ -12,20 +15,25 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return  res.send(JSON.stringify(books,null,4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  return res.status(300).send(books[isbn]);
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/m',function (req, res) {
+  const author = req.params.author;
+  keys = Object.keys(books);
+  //usar keys para iterar
+  let validbooks = books.filter((book)=>{
+    return (book[key].author === author)
+  });
+
+  return res.send(validbooks);
 });
 
 // Get all books based on title
